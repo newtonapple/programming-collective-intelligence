f8ba6c11be43bc7a69db76f90e8e99a091867ad2
David Dai <david@affinitylabs.com>
2008-09-07 16:20:41 -0700

    more rdoc tweaks.

--- lib/ch2/similarity.rb
+++ lib/ch2/similarity.rb
@@ -1,27 +1,31 @@
 module Similarity
   
   # returns common_times shared by x and y
-  # --
+  #--
   # NOTE: common_items can be found through generic traversal of the smaller set
   # for example:
   #   if x.size < y.size
   #   we can traverse x and generate stats for andy item such that x[item] & y[item] exists
-  # This could be faster when x.size and y.size is large
+  # Th  is could be faster when x.size and y.size is large
   def self.common_items(x, y)
     x.keys & y.keys
   end
   
   
-  # == Ecludiean Distance
-  # Ecludiean distance measures the similarity of two subjects by measuring the closeness of their preference scores.
-  # The heuristic is calculated by summing up the differences of two subjects' scores.
-  # Since we want a positive number we'll square those differences.
-  # Two subjects are more similar if their sum of score differences is smaller.
-  # * Formula:
+  # === Ecludiean Distance
+  # * Ecludiean distance measures the similarity of two subjects by measuring the closeness of their preference scores.
+  # * The heuristic is calculated by summing up the differences of two subjects' scores.
+  # * Since we want a positive number we'll square those differences.
+  # * Two subjects are more similar if their sum of score differences is smaller.
+  #
+  # ==== Formula:
   #     d = √( ∑(x-y)^2 )
-  
-  # x, y => {"item" => score}
-  # sum ( (Xi - Yi)^2 ) => sum of squared score differences
+  #
+  # ==== Parmamters:
+  # <tt>x, y</tt>:: {"item" => score}
+  #
+  # ==== Returns:
+  # √( ∑(x-y)^2 )
   def self.ecludiean_distance( x, y )    
     common_items(x, y).inject(0) do |sum, item|
       # only count the scores if both subjects share something in common
@@ -31,10 +35,13 @@ module Similarity
   end
   
   
-  # Distance similarity will always return a number b/w (0, 1]
+  # 
   # Since we want a higher score to represent more similiarity between the subjects, we'll simpliy take the inverse the ecludiean_distance. 
   # And, to prevent division by zero we'll add 1 to the denominator.
-  # * sim_distance() in book.
+  # <em>Book Reference</em>:: <tt>sim_distance()</tt> in book.
+  # ==== Returns:
+  # * Distance similarity will always return a number b/w (0, 1]
+  # * 1 / ( 1 + √( ∑(x-y)^2 ) )
   def self.distance_similarity( x, y )
     # note you don't need a root here, since we are only looking for inverse difference
     # as long as we are comparing everything to the same scale we're ok
@@ -42,18 +49,19 @@ module Similarity
   end
   
   
-  # == Pearson Correlation Score
+  # === Pearson Correlation Score
   # * Pearson Correlation Score is also known as the "best-fit line" heurestic.
-  # * Pearson Correlation summarizes the linear relationships between two subjects
+  # * Pearson Correlation summarizes the linear relationships between two subjects.
   # * It corrects "grade inflation" e.g. someone who is a particularly harsh or generous critic.
-  # Pearson Score is between [-1, 1]::
-  # 1. -1: implies rating/scores of x & y are complete opposite
-  # 2. 1: implies x & y rated exactly the same
-  # 3. 0: x & y not related
-  
-  # * Formula:
+  #
+  # ==== Formula:
   #     r = ( ∑x∙y - ∑x∙∑y/n ) / √( (∑x^2 - (∑x)^2/n) ∙ (∑y^2 - (∑y)^2/n) )
-  
+  #
+  # ==== Returns:
+  # Pearson Score is between [-1, 1]:
+  # 1. -1: implies rating/scores of x & y are complete opposite
+  # 2.  1: implies x & y rated exactly the same
+  # 3.  0: x & y not related
   def self.pearson_similarity( x, y )
     common_items = self.common_items(x, y)
     return 0 if common_items.empty? # nothing is shared
@@ -78,7 +86,7 @@ module Similarity
   end
 
   class << self
-     alias :best_fit_similarity :pearson_similarity
+     alias :linear_similarity :pearson_similarity
   end
   
   
